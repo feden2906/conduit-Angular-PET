d75698301945654f5a7bda21e94766cd6383cc3e
Fedenko Maksym <feden2906@gmail.com>
2021-02-04 23:59:41 +0200

    login component

---
+++ src/app/login/services/login.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {FullUser} from '../../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  baseUrl = 'https://conduit.productionready.io/api';
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  loginUser(user): Observable<FullUser> {
+    const body = {user};
+    console.log(body);
+    return this.httpClient.post<FullUser>(`${this.baseUrl}/users/login`, body);
+  }
+}
